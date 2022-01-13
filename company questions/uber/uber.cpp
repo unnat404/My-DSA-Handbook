7894ecf423d2bf928cc1df21217c13c95c20018c
unnat <53619178+unnat404@users.noreply.github.com>
2022-01-14 01:05:20 +0530

    uber #2

--- company questions/uber/uber.cpp
+++ company questions/uber/uber.cpp
@@ -1,8 +1,11 @@
 
 /* 
 Uber Questions Lists:
-- find all subsets that sum up to k ::  https://www.codingninjas.com/codestudio/problems/return-subsets-sum-to-k_759331
-- rotten oranges :: 
+1- find all subsets that sum up to k ::  https://www.codingninjas.com/codestudio/problems/return-subsets-sum-to-k_759331
+2- rotten oranges :: 
+3- meeting :: https://www.codingninjas.com/codestudio/problems/meeting_1376415
+4- house robbers
+5- running median :: https://www.geeksforgeeks.org/median-of-stream-of-integers-running-integers/
 */
 //===================================================================================================
 //find all subsets that sum up to k ::  https://www.codingninjas.com/codestudio/problems/return-subsets-sum-to-k_759331
@@ -104,3 +107,129 @@ public:
 //===================================================================================================
 
 // https://leetcode.com/discuss/interview-question/1272223/uber-oa-questions-codesignal-test-2021
+
+//===================================================================================================
+
+// meeting :: https://www.codingninjas.com/codestudio/problems/meeting_1376415
+
+/*
+Problem Statement
+Ninja is organizing a meeting in an office that starts at a time ‘0’ and will end at a time ‘LAST’.
+There are ‘N’ presentations that are going to take place in that meeting whose start and end times are given, i.e., ‘ith’ presentation starts at ‘START[i]’ and ends at ‘END[i]’. The presentations don’t overlap with each other. Ninja wants to reschedule at most ‘K’ presentations keeping the original order intact so that the longest period in which there is no presentation scheduled during the meeting is maximized.
+Since Ninja is busy with other office work, your task is to help Ninja to reschedule at most ‘K’ presentations.
+
+Constraints:
+1 <= T <= 10
+1 <= N <= 5000
+0 <= K <= N
+0 <= LAST <= 10^9
+START[i] < END[i], for 1 <= i <= N.
+END[i] <= START[i+1], for 1 <= i < N.
+Time Limit: 1 second
+----------------------
+Sample Input 1:
+1
+2 1 4
+0 1
+2 3
+Sample Output 1:
+2
+Explanation For Sample Output 1:
+If we reschedule the second meeting from 2 to 1, then the duration from 2 to 4 will be free, and it is the longest among all possible answers.
+----------------------
+Sample Input 2:
+1
+3 0 11
+2 4
+4 5
+8 10          
+Sample Output 2:
+3
+*/
+//approach 1: Brute Force Approach    : O(n*n)
+//approach 2: sliding window approach : O(n)
+/*
+    Time Complexity     :   O(N)
+    Space Complexity    :   O(N)
+    Where 'N' is the number of the presentations.
+*/
+
+int maxInterval(int t, vector<int> &start, vector<int> &end, int k) {
+	// Create a vector to store the time duration in which there is no presentation.
+	vector<int> emptySlots;
+	// Fill the vector 'emptySlots'.
+	int prevEnd = 0;
+	for (int i = 0; i < start.size(); i++) {
+		emptySlots.push_back(start[i] - prevEnd);
+		prevEnd = end[i];
+	}
+	emptySlots.push_back(t - prevEnd);
+	int len = emptySlots.size();
+	// Declare a variable to store the final answer.
+	int ans = 0;
+	// Declare a variable to store the sum of current window.
+	int sum = 0;
+	// Find the sum of 1st K + 1 window.
+	for (int i = 0; i < k + 1 && i < len; i++) {
+		sum = sum + emptySlots[i];
+	}
+	// Update 'ans'.
+	ans = sum;
+	// Find the sum of all K + 1 window and update 'ans' with the maximum.
+	for (int i = k + 1; i < len; i++) {
+		sum = sum - emptySlots[i - (k + 1)] + emptySlots[i];
+		ans = max(sum, ans);
+	}
+	return ans;
+}
+
+//===================================================================================================
+// no of unique paths :: https://www.codingninjas.com/codestudio/problems/total-unique-paths_1081470
+
+// total unique paths: from (0,0)->(m-1,n-1)
+// Returns count of possible paths to reach cell at
+// row number m and column  number n from the topmost
+// leftmost cell (cell at 1, 1)
+int numberOfPaths(int m, int n)
+{
+    // Create a 2D table to store results of subproblems
+    int count[m][n];
+
+    // Count of paths to reach any cell in first column is 1
+    for (int i = 0; i < m; i++)
+        count[i][0] = 1;
+
+    // Count of paths to reach any cell in first row is 1
+    for (int j = 0; j < n; j++)
+        count[0][j] = 1;
+
+    // Calculate count of paths for other cells in
+    // bottom-up manner using the recursive solution
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++)
+
+            // By uncommenting the last part the code calculates the total
+            // possible paths if the diagonal Movements are allowed
+            count[i][j] = count[i - 1][j] + count[i][j - 1]; //+ count[i-1][j-1];
+    }
+    return count[m - 1][n - 1];
+}
+
+// same pblm with obstacles:: https://www.geeksforgeeks.org/unique-paths-in-a-grid-with-obstacles/ 
+
+//===================================================================================================
+
+//===================================================================================================
+
+//===================================================================================================
+
+//===================================================================================================
+
+//===================================================================================================
+//===================================================================================================
+//===================================================================================================
+//===================================================================================================
+//===================================================================================================
+//===================================================================================================
+//===================================================================================================
+//===================================================================================================
