00022df66c07a37dca65118d6567b098a115361d
unnat <53619178+unnat404@users.noreply.github.com>
2022-03-01 13:32:18 +0530

    uber hacktag-prev #1

--- company questions/uber/uber_bs.cpp
+++ company questions/uber/uber_bs.cpp
@@ -13,7 +13,7 @@ Question List:
 10- uber oa || 14/01/2022 || question 4 (convert base2 to base6) : repeated
 11- uber oa || 14/01/2022 || 
 12- uber oa || 14/01/2022 || modification of burst balloons
-13- 
+13- uber hacktag :: https://codeforces.com/problemset/problem/830/A
 14- 
 */
 
@@ -524,3 +524,90 @@ public:
 // leetcode :: brust balloons ka modification 
 
 //=================================================================================================
+// Uber hacktag :: https://codedaily.in/uber-hack-tag-questions/
+// https://codeforces.com/problemset/problem/830/A
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define P pair<int,int>
+#define V vector
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define forstl(i, s)  for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+// #pragma comment(linker, '/STACK:200000')        //prevent stack overflow ..STACK Limiter
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define time() cerr << "Time : " << (double)clock() / (double)CLOCKS_PER_SEC << "s\n" 
+#define db(...) __f(#__VA_ARGS__, __VA_ARGS__)
+const int MOD=1e9+7;
+const int MOD2=998244353;
+ld PI=2*acos(0.0);
+ 
+template <typename T> T gcd(T a, T b){return (b==0)?a:gcd(b,a%b);}
+template <typename T> T lcm(T a, T b){return a*(b/gcd(a,b));}   
+template <typename T> T mod_exp(T b, T p, T m){T x = 1;while(p){if(p&1)x=(x*b)%m;b=(b*b)%m;p=p>>1;}return x;}
+template <typename T> T invFermat(T a, T p){return mod_exp(a, p-2, p);}
+template <typename T> T exp(T b, T p){T x = 1;while(p){if(p&1)x=(x*b);b=(b*b);p=p>>1;}return x;}
+string tostring(int number){stringstream ss; ss<<number; return ss.str();}
+int toint(const string &s) {stringstream ss; ss << s; int x; ss >> x; return x;}
+ 
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << '\n'; }
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr(names + 1, ',');
+    cout.write(names, comma - names) << " : " << arg1 << " | "; __f(comma + 1, args...);
+}  
+ 
+const int maxm = 2*1e3+5;
+int a[maxm],b[maxm];
+int n,k,p;
+int dp[maxm][maxm];
+ 
+int checker(int pos,int key)
+{
+	if(pos >= n) return -1e12;
+	if(key >= k) return 1e12;
+	if(dp[pos][key]!=-1) return dp[pos][key];
+	int ans = 0;
+	int op1 = checker(pos,key+1);
+	int time = abs(a[pos] - b[key]) + abs(b[key] - p);
+	int op2 = max(time,checker(pos+1,key+1));
+	ans = min(op1,op2);
+	return dp[pos][key] = ans;
+}
+ 
+void solve()
+{
+	cin>>n>>k>>p;
+	memset(dp,-1,sizeof dp);
+	for(int i=0;i<n;i++) cin>>a[i];
+	sort(a,a+n);// sort people positions
+	for(int i=0;i<k;i++) cin>>b[i];
+	sort(b,b+k);// sort keys(or battery) positions
+	cout<<checker(0,0)<<endl;
+}
+ 
+int32_t main()
+{
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    #ifndef ONLINE_JUDGE                    //file start
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);   
+    #endif                                  //file end
+    // int cases=1;
+    // int t; cin>>t;
+    // while(t--)
+    solve();
+    return 0;
+}
+ 
+// 1LL check ?
