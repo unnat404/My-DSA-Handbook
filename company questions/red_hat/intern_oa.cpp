cd9aef8367a08851201a08d7bdb4fddf4bb7d858
unnat <53619178+unnat404@users.noreply.github.com>
2022-11-23 01:39:29 +0530

    company qns: titan || red_hat

--- company questions/red_hat/intern_oa.cpp
+++ company questions/red_hat/intern_oa.cpp
@@ -2,7 +2,7 @@
 
 01. https://leetcode.com/problems/maximum-number-of-non-overlapping-palindrome-substrings/description/
 02. https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/
-03. 
-04. 
+03. https://discuss.codechef.com/t/hackerrank-recycling-cartridges/85101
+04. https://stackoverflow.com/questions/72859836/count-all-paths-in-a-tree-that-have-cost-0-modulo-k
 
 */
---
+++ company questions/titan/titan_oa.cpp
@@ -0,0 +1,9 @@
+/*
+Titan OA Kanisht (19/11/22)
+---------------------------
+- https://www.geeksforgeeks.org/split-the-given-array-into-k-sub-arrays-such-that-maximum-sum-of-all-sub-arrays-is-minimum/
+- https://www.geeksforgeeks.org/range-lcm-queries/
+-  https://p.ip.fi/_J0Z (my soln)
+
+*/
+
